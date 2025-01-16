c2ff366c4d9beb3b4eb6b815f2c24d2c2bd5dd8b
Harish Rusum <harish.rusum@gmail.com>
2025-01-16 21:27:24 +0530

    initial commit

---
+++ swap.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+mv hyprpaperAlternate.conf temp.conf
+mv hyprpaper.conf hyprpaperAlternate.conf
+mv temp.conf hyprpaper.conf
+
+mv colors.conf temp.conf
+mv colorsAlternate.conf colors.conf
+mv temp.conf colorsAlternate.conf
